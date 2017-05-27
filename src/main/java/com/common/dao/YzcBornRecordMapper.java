eff6a1bc30575477406fcffe761028a14285522c
lcppcl <786896996@qq.com>
2017-05-27 16:37:17 +0800

    Update YzcBornRecordMapper.java

--- src/main/java/com/common/dao/YzcBornRecordMapper.java
+++ src/main/java/com/common/dao/YzcBornRecordMapper.java
@@ -7,8 +7,6 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
  * <p>
   * 出生记录表 Mapper 接口
  * </p>
- *
- * @author wangfj
  * @since 2017-04-16
  */
 public interface YzcBornRecordMapper extends BaseMapper<YzcBornRecord> {
